ae1802db12ba96934665940c95690c1ba6edf489
8urger <93502633+8urger@users.noreply.github.com>
2021-11-24 13:42:07 -0700

    Update test.py

--- sudoku/test.py
+++ sudoku/test.py
@@ -1,47 +1,84 @@
+import pprint
+import numpy as np
 
-yourBoard = [[0 for x in range(9)] for y in range(9)]
-sector = [[0, 0, 0, 0, 0, 0, 0, 0, 0] for w in range(9)]
-print(yourBoard[0][1])
-print(yourBoard)
-print(sector[8])
-row = [0, 0, 0]
-print(row)
-for i in range(3):
-	row[i] = yourBoard[1][i]
-print(row)
-column = [[0, 0, 0] for i in range(3)]
-print(column)
-for i in range(3):
-	for j in range(3):
-		column[i][j] = yourBoard[j][i]
-print(column)
 
-print("Here are the sectors")
-print(sector)
-x=0
+yourBoard = [[0 for a in range(4)] for b in range(4)]
+for i in range(4):
+	print(yourBoard[i])
+actualBoard = [[[0 for a in range(4)] for c in range(4)] for c in range(4)]
 
-#for a in range(0, 9, 3):
-#	for b in range(0, 9, 3):
-a = 0
-b = 0
-x=0
-for p in range(0, 9, 3):
-	
-	for n in range(0, 9, 3):
-		b = -1
-		for i in range(3):
+print("")
+for i in range(4):
+	for j in range(4):
+		for h in range(4):
+			actualBoard[i][j][h] = h+1
 
-			for j in range(3):
-				x+=1
-				sector[a][b] = yourBoard[(i+p)][(j+n)]
-				b+=1
-				print(b)
-		a+=1
-	#b+=3
+sec1 = [1, 0, 3, 4]
+sec2 = [3, 0, 1, 2]
+sec3 = [4, 1, 2, 3]
+sec4 = [2, 3, 4, 1]
 
-print(p)
-print(n)
+pprint.pprint(actualBoard)
+
+print("3D array element")
+
+print(actualBoard[0][0][0])
+
+print("")
+
+yourBoard[0][0] = 1
+yourBoard[0][1] = 0
+yourBoard[0][2] = 3
+yourBoard[0][3] = 0
+yourBoard[1][0] = 3
+yourBoard[1][1] = 4
+yourBoard[1][2] = 1
+yourBoard[1][3] = 2
+yourBoard[2][0] = 4
+yourBoard[2][1] = 1
+yourBoard[2][2] = 2
+yourBoard[2][3] = 3
+yourBoard[3][0] = 2
+yourBoard[3][1] = 3
+yourBoard[3][2] = 4
+yourBoard[3][3] = 1
+x = 0
+for i in range(4):
+	print(yourBoard[i])
+
+
+
+for i in range(4):
+	for j in range(4):
+		if (yourBoard[i][j]!=0):
+			q = yourBoard[i][j]
+			#actualBoard[i][j][(q-1)]=0
+			for a in range(4):
+				actualBoard[i][j][a] = 0
+
+# Pass the cordinates for the cell to be solved(i, j)
+# Pass the sector the cell is in(sector_x)
+# Now I can say that coordinate ij == naught(sector, i, j)
+def solver(i, j, x):
+	#for loop iterating through all 
+	#values in the naught, if !=0, add to possibilities
+	pass
+
+#isolate the sector that needs to be solved to pass to solver
+print("And now this test")
+for i in range(4):
+	for j in range(4):
+		if (yourBoard[i][j]==0):
+			if ((0<=i<2) and (0 <=j<2)):
+				solver(i, j, sec1)
+			elif((0<=i<2) and (2<=j<4)):
+				print("sector2")#solver(i, j, sec2)
+			elif((2<=i) and (0<=j<2)):
+				print("Sector3")#solver (i, j, sec3)
+			else:
+				print("sector4")#solver(i, j, sec4)
+print("Test complete")
+pprint.pprint(actualBoard)
+print(q)
 print(x)
-print(a)
-print(b)
-print(sector)
+
