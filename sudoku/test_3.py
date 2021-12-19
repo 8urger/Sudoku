b6b72318b53ff1a1754be6a957fec69eb9c7fe5f
8urger <93502633+8urger@users.noreply.github.com>
2021-12-19 09:09:19 -0700

    Update test_3.py

--- sudoku/test_3.py
+++ sudoku/test_3.py
@@ -56,6 +56,55 @@ deleteArray = [0, 1, 3, 0, 0, 5]
 print(deleteArray)
 for y in range(len(deleteArray)):
 	if (deleteArray[y] == 0):
-		print("Hi")
-		y =- 1
-		print(i)
+		pass
+
+
+print("And now the board")
+for i in range(9):
+	print(yourBoard[i])
+
+toSolve = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+def solver(yourBoard, full, i, j):
+	
+	print("Here is i and j")
+	print(i)
+	print(j)
+	for m in range(len(full)):
+		if (yourBoard[i][m]!=0):
+			x = yourBoard[i][m]
+			for i in range(len(full)):
+				if (full[i] == x):
+					print("hi")
+					
+
+	def compare_num(self, x):
+		cur_node = self.head
+		while cur_node:
+			if (cur_node.data == x):
+				return True
+
+	for n in range(9):
+		if (yourBoard[n][j]!=0):
+			x = yourBoard[n][j]
+			for i in range(len(full)):
+				if (full[j]==x):
+					full[j]=0
+
+	print(full)
+	x=0
+	for i in range(len(full)):
+		if (full[i]==0):
+			x=x+1
+			
+	print(x)
+	print(yourBoard[i][j])
+	return full
+x=0
+#for i in range(9):
+#	for j in range(9):
+#		if (yourBoard[i][j]==0):
+#			solver(yourBoard, i, j)
+full = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+solver(yourBoard, full, 0, 0)
+print(full)
