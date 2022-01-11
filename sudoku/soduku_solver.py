44e4401ef9368baf6d9c0a3b4a485edc5160266c
8urger <93502633+8urger@users.noreply.github.com>
2022-01-11 10:45:12 -0700

    Update soduku_solver.py

--- sudoku/soduku_solver.py
+++ sudoku/soduku_solver.py
@@ -1,5 +1,5 @@
 import boardBuilder
-import numpy as np
+import soduku_populate
 
 def main():
 	answer = 'z'
@@ -8,12 +8,16 @@ def main():
 		print("What would you like to do next?")
 		print("Press i to populate a cell.")
 		print("Press p to print your Sudoku table.")
+		print("Press 1-9 to load pre-programmed puzzles in.")
+		print("Press r to populate all columns, rows, and sectors")
+		print("Press s to solve!")
 		answer = input("Press q to quit.")
 		boardBuilder.menu(answer, yourBoard)
 
 if __name__ == "__main__":
 	yourBoard = [[0 for a in range(9)] for b in range(9)]
 	main()
+
 
 
 
